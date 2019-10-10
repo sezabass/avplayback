f86116a11261032e5b38779f2dada88afcdec2d1
Cesar Castro <cesarcastrocesar@gmail.com>
2019-10-09 23:19:48 -0300

    Add a generic ScreenState object for handling request states on the UI

---
+++ app/src/main/kotlin/com/looke/avplayback/core/domain/ScreenState.kt
@@ -0,0 +1,7 @@
+package com.looke.avplayback.core.domain
+
+sealed class ScreenState<out T> {
+    data class Success<T>(val data: T) : ScreenState<T>()
+    data class Error(val message: String? = null) : ScreenState<Nothing>()
+    object Loading : ScreenState<Nothing>()
+}
