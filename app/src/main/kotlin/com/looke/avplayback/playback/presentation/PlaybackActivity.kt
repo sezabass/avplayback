db145e35329dd5ef3dd88000eff3fd755b718f6e
Cesar Castro <cesarcastrocesar@gmail.com>
2019-10-10 09:54:46 -0300

    Setup audio and video on PlaybackActivity

--- app/src/main/kotlin/com/looke/avplayback/playback/presentation/PlaybackActivity.kt
+++ app/src/main/kotlin/com/looke/avplayback/playback/presentation/PlaybackActivity.kt
@@ -3,13 +3,18 @@ package com.looke.avplayback.playback.presentation
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.looke.avplayback.R
+import com.looke.avplayback.databinding.ActivityPlaybackBinding
 import com.looke.avplayback.videos.domain.LookeVideo
+import kotlinx.android.synthetic.main.activity_playback.*
 
 class PlaybackActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlaybackBinding
 
     private var mediaPlayer: MediaPlayer? = null
     private val video by lazy {
@@ -18,11 +23,34 @@ class PlaybackActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mediaPlayerStart()
+
+        setupBinding()
+        binding.playbackTitle.text = video.name
+
+        setupVideo()
+        setupAudio()
     }
 
-    private fun mediaPlayerStart() {
-        mediaPlayer = MediaPlayer.create(this, Uri.parse(video.videoURL))
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_playback)
+        binding.lifecycleOwner = this
+    }
+
+    private fun setupVideo() {
+        vv.setVideoURI(video.videoURL)
+        vv.setOnCompletionListener {
+            it.start()
+        }
+        vv.setOnPreparedListener {
+            it.start()
+        }
+    }
+
+    private fun setupAudio() {
+        mediaPlayer = MediaPlayer.create(this, video.audioURL)
+        mediaPlayer?.setOnCompletionListener {
+            vv.stopPlayback()
+        }
         mediaPlayer?.start()
     }
 
